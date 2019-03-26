d87bd0460e3a0f401eb03c67c80da8242b0ecfe3
Dagmar Stránská <ds@creox.cz>
2019-03-26 23:18:57 +0100

    final DASA fixes

--- .skin/frontend/src/js/app.js
+++ .skin/frontend/src/js/app.js
@@ -1,6 +1,56 @@
 import "../css/app.scss";
+import counterUp from 'counterup2'
 
 
-$( ".hamburger" ).click(function() {
-  $( this ).toggleClass( "is-active" );
-});
+const el_employee = document.querySelector('.counter-employee')
+const el_company = document.querySelector('.counter-company')
+const el_exp = document.querySelector('.counter-exp')
+var counter = false
+
+$(document).ready(function () {
+
+  $(".hamburger").click(function () {
+    $(this).toggleClass("is-active");
+  });
+
+
+  function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
+
+  if ($('.counter').length) {
+
+    window.onscroll = function () {
+      if (isScrolledIntoView('.counter')) {
+        if (counter === false) {
+          counterUp(el_employee, {
+            duration: 5000,
+            delay: 16,
+          })
+          counterUp(el_company, {
+            duration: 5000,
+            delay: 16,
+          })
+          counterUp(el_exp, {
+            duration: 5000,
+            delay: 16,
+          })
+
+
+          counter = true;
+        }
+
+      }
+    }
+
+    window.onscroll()
+  }
+})
+
+
--- .skin/frontend/webpack.config.js
+++ .skin/frontend/webpack.config.js
@@ -2,37 +2,52 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const webpack = require('webpack');
 
 module.exports = {
-    entry: { main: './src/js/app.js' },
-    output: {
-        path: __dirname + '/../../www/skin/frontend',
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
-            }
-        },{
-            test: /\.scss$/,
-            use: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: ['css-loader','sass-loader']
-            })
-        }]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'windows.jQuery': 'jquery',
-        }),
-        new ExtractTextPlugin({filename: 'style.css'})
-    ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.js'
+  entry: {main: './src/js/app.js'},
+  output: {
+    path: __dirname + '/../../www/skin/frontend',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test:
+          require.resolve('jquery'),
+        use:
+          [{
+            loader: 'expose-loader',
+            options: '$'
+          },{
+            loader: 'expose-loader',
+            options: 'jQuery'
+          }]
+      }]
+  },
+  plugins:
+    [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+      }),
+      new ExtractTextPlugin({filename: 'style.css'})
+    ],
+  resolve:
+    {
+      alias: {
+        vue: 'vue/dist/vue.js'
+      }
     }
 }
